4f830ece34530457cbea36c5fc32269f5b6baef7
Balázs <balazs@balazs-Inspiron-N7010.(none)>
2012-11-15 11:08:17 +0100

    barcelona szöveg eto szöveg + képek menü css levéve
    Signed-off-by: Balázs <balazs@balazs-Inspiron-N7010.(none)>

---
+++ php/fcbarcelona.php
@@ -0,0 +1,45 @@
+		<?php $lang = ($_SESSION['lang'] == 'en' ? '/en' : ''); ?>
+		<!-- CONTENT SIDE -->
+		<div class="content_side clearfix">
+		
+			<div class="box_data"><div class="bgr_top"><div class="bgr_bot clearfix">
+				<?php if($_SESSION['lang'] == 'en'){ ?>
+					<div class="ful_rolunk">About us</div>
+					<div class="clear"></div>
+					<div class="article">
+						A program a nagyközönségnek tervezett (55 éves korig)… akik készek tanulni és tapasztalatot akarnak szerezni kívülről és belülről a <strong>világ legjobb klubjától az FC Barcelonától.</strong>Ne hagyd ki ezt a különleges lehetőséget, tanulj és tapasztalj az FC Barcelonánál, a világ egyik leghíresebb és legsikeresebb klubjánál.<br />
+						<br />
+						Amit kínálunk:<br />
+						<br />
+						5 napon át, 90 perces edzés (reggel) az FC Barcelona proﬁ edzőivel<br />
+						Hivatalos csoport képek<br />
+						Víz fogyasztás minden edzés alatt<br />
+						FC Barcelona Stadion, Múzeum látogatása idegenvezetéssel<br />
+						<br />
+						Szállás: 3*,4* illetve 5* hotelebken (minden szálloda a stadion mellett, vagy néhány perc sétára található). További részletekről érdeklődjön <a href="<?php echo $lang; ?>/kapcsolat">irodánkban, illetve elérhetőségeinken</a>. Valamint kérheti részletes tájékoztató füzetünket.<br />
+						<br />
+						<br />
+						<img src="img/rolunk_foto.jpg" alt="" />
+					</div>
+				<?php }else{ ?>
+					<div class="ful_rolunk">Különlöges sportélmény Barcelonában</div>
+					<div class="clear"></div>
+					<div class="article">
+						A program a nagyközönségnek tervezett (55 éves korig)… akik készek tanulni és tapasztalatot akarnak szerezni kívülről és belülről a <strong>világ legjobb klubjától az FC Barcelonától.</strong>Ne hagyd ki ezt a különleges lehetőséget, tanulj és tapasztalj az FC Barcelonánál, a világ egyik leghíresebb és legsikeresebb klubjánál.<br />
+						<br />
+						Amit kínálunk:<br />
+						<br />
+						5 napon át, 90 perces edzés (reggel) az FC Barcelona proﬁ edzőivel<br />
+						Hivatalos csoport képek<br />
+						Víz fogyasztás minden edzés alatt<br />
+						FC Barcelona Stadion, Múzeum látogatása idegenvezetéssel<br />
+						<br />
+						Szállás: 3*,4* illetve 5* hotelekben (minden szálloda a stadion mellett, vagy néhány perc sétára található). További részletekről érdeklődjön <a href="<?php echo $lang; ?>/kapcsolat">irodánkban, illetve elérhetőségeinken</a>. Valamint kérheti részletes tájékoztató füzetünket.<br />
+						<br />
+						<br />
+						<img src="img/rolunk_foto.jpg" alt="" />
+				<?php } ?>				
+			</div></div></div>
+			
+		</div>
+		<!-- CONTENT SIDE -->
