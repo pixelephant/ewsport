f50baa99ee6e713c4936e74b58f32960bfa55530
Balázs <balazs@balazs-Inspiron-N7010.(none)>
2012-11-23 13:20:56 +0100

    győri eto vip angol szöveg
    Signed-off-by: Balázs <balazs@balazs-Inspiron-N7010.(none)>

--- php/gyorietovip.php
+++ php/gyorietovip.php
@@ -4,29 +4,29 @@
 		
 			<div class="box_data"><div class="bgr_top"><div class="bgr_bot clearfix">
 				<?php if($_SESSION['lang'] == 'en'){ ?>
-					<div class="ful_rolunk">Béreljen VIP páholyt és hirdetési felületet a Győri ETO FC stadionjában</div>
+					<div class="ful_rolunk">VIP boxes in the Győri ETO FC Stadium are for sale!</div>
 					<div class="clear"></div>
 					<div class="article">
-						<h2>Béreljen VIP páholyt és hirdetési felületet a Győri ETO FC stadionjában</h2>
-						<p>4.000.000,-Ft + áfa - A kapuval és a tizenhatos vonallal egy magasságban</p>
-						<p>5.000.000,-Ft + áfa - A térfél közepe, a tizenhatos vonal és a kezdőkőr közötti terület</p>
-						<p>6.000.000,-Ft + áfa - A kezdőkörrel szemben a pálya közepén lévő páholy</p>
+						<h2>VIP boxes in the Győri ETO FC Stadium are for sale!</h2>
+						<p>4.000.000,-Ft + VAT - At the goal till the penalty area.</p>
+						<p>5.000.000,-Ft + VAT - From the mid-half – penalty area – to the center circle.</p>
+						<p>6.000.000,-Ft + VAT - Opposite to the center circle. </p>
 						<br />
 						<div class="eto">
 							<img src="img/gyorietovip/9.jpg" alt="" />
 							<img src="img/gyorietovip/2.jpg" alt="" />
 						</div>
-						<h3>Szolgáltatások</h3>
+						<h3>Services</h3>
 						<br />
 						<ul>
-							<li>minden kategóriához azonos catering szolgáltatás,( hideg,meleg ételek, sör,bor,üdítő italok, ásvány víz, tea. kávé korlátlan fogyasztása)</li>
-							<li>személyzet a catering felszolgálásához</li>
-							<li>lehetőség üzleti partnerek, fogadásához a VIP-ben</li>
-							<li>minden páholyhoz 16 db belépő, minden hazai mérkőzéshez, bajnoki és kupa egyaránt</li>
-							<li>négy autó belső parkolási lehetősége, a VIP alatt</li>
-							<li>a cég logójának, ( tábla reklámjának) kihelyezése a stadionba a TV által jól látható helyre</li>
+							<li>All categories include same quality catering service ( cold and hot meals, beer, wine, refreshers, mineral water, tea, coffee are unlimited )</li>
+							<li>Staff for the catering service</li>
+							<li>Possibility to entertain business partners in the VIP box</li>
+							<li>All boxes include 16 tickets for all domestic matches ( Championship and Cup matches )</li>
+							<li>Possibility for 4 cars  to park under the VIP</li>
+							<li>Representing the company’s name, logo, or commercial on board in the stadium</li>
 						</ul>
-						<p>További részletekről érdeklődjön <a href="<?php echo $lang; ?>/kapcsolat">irodánkban, illetve elérhetőségeinken</a>.</p>
+						<p>For further details please <a href="<?php echo $lang; ?>/kapcsolat">contact us</a>.</p>
 						<div class="eto">
 							<img src="img/gyorietovip/3.jpg" alt="" />
 							<img src="img/gyorietovip/4.jpg" alt="" />
